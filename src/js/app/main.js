bf603a26fb493c13b7955203fe33e88dd10fd177
bpa <bpa@ugraweb.ru>
2017-12-19 19:37:57 +0500

    Реализация внутренних страниц

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -17,7 +17,7 @@
         slidesPerView: 'auto',
         navigation: {
           nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          prevEl: '.swiper-button-prev'
         },
         pagination: {
           el: '.swiper-pagination',
@@ -35,6 +35,48 @@
         pagination: {
           el: '.swiper-pagination',
           clickable: true
+        }
+      })
+    }
+
+    // слайдер main-second
+    {
+      new Swiper('.js-videos', {
+        loop: false,
+        slidesPerView: 'auto',
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+      })
+    }
+
+    // слайдер partners
+    {
+      new Swiper('.js-partners', {
+        loop: true,
+        slidesPerView: 'auto',
+        navigation: {
+          nextEl: '.partners-next',
+          prevEl: '.partners-prev'
+        }
+      })
+    }
+
+    // range
+    {
+      const $range = $('.js-range')
+      const getData = ((needed) => { return $range.data(needed) })
+      const getValue = ((needed) => { return parseInt($(getData(needed)).val()) })
+      $range.slider({
+        values: [ getValue('target-1'), getValue('target-2') ],
+        range: true,
+        min: getData('min'),
+        max: getData('max'),
+        step: getData('step'),
+        slide: (event, ui) => {
+          const selector = 'target-' + (ui.handleIndex + 1)
+          $(getData(selector)).val(ui.value)
         }
       })
     }
